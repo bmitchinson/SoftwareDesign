6eb7dca789811fe989cc0af84ef52a5483f024fd
Ben Mitchinson <benjamin-mitchinson@uiowa.edu>
2018-10-05 19:10:30 -0500

    Start Hangman

--- oral_exam1/S21_HangmanGUI_Easy/src/HangmanGame.java
+++ oral_exam1/S21_HangmanGUI_Easy/src/HangmanGame.java
@@ -1,2 +1,11 @@
-public class HangmanGameFrame {
+import java.security.SecureRandom;
+
+public class HangmanGame {
+    String goal;
+    SecureRandom randomGen = new SecureRandom();
+
+    public HangmanGame(String[] words) {
+        goal = words[randomGen.nextInt(words.length)];
+        System.out.println("Goal:"+goal);
+    }
 }
