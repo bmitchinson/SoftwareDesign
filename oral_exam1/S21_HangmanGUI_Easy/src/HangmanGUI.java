7de5357ab193ff2b351ffedc4d2dd89cdcf141b1
Ben Mitchinson <benjamin-mitchinson@uiowa.edu>
2018-10-05 19:10:17 -0500

    Hangman Start

---
+++ oral_exam1/S21_HangmanGUI_Easy/src/HangmanGUI.java
@@ -0,0 +1,8 @@
+public class HangmanGUI {
+
+    public static void main(String[] args) {
+        String[] words = {"Cat", "Dog", "Man", "Sky", "Dim", "Big"};
+        HangmanGame game = new HangmanGame(words);
+    }
+
+}
---
+++ oral_exam1/S21_HangmanGUI_Easy/src/HangmanGame.java
@@ -0,0 +1,2 @@
+public class HangmanGameFrame {
+}
