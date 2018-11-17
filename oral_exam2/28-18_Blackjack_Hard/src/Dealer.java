fa97d59bcef0286afe53152af80187d4119c3d04
Ben Mitchinson <benjamin-mitchinson@uiowa.edu>
2018-11-17 17:28:09 -0600

    Unstuck

--- oral_exam2/28-18_Blackjack_Hard/src/Dealer.java
+++ oral_exam2/28-18_Blackjack_Hard/src/Dealer.java
@@ -6,26 +6,63 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Formatter;
+import java.util.Scanner;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
+// TODO: JDoc that's transparent about the reference of "fig28_11_14"
 public class Dealer extends JFrame {
-    private JTextArea displayArea;
-    private ObjectOutputStream outputOne;
-    private ObjectOutputStream outputTwo;
-    private ObjectInputStream inputOne;
-    private ObjectInputStream inputTwo;
-    private Player[] players;
+    private final static int P_ONE = 1;
+    private final static int P_TWO = 2;
+    // TODO: Create a "Hand" class to represent cards, but also their total.
+    //       Dynamically factors in the ace based on what's held. Constructed with
+    //       an initial deal. Use this instead of the Card array below.
     private Card[] cards;
+    private JTextArea outputArea;
+    private Player[] players;
     private ServerSocket server;
-    private Socket connectionOne;
-    private Socket connectionTwo;
-    private int playersConnected = 0;
+    private int currentPlayer;
+    private ExecutorService runGame;
+    private Lock gameLock;
+    private Condition otherPlayerConnected;
+    private Condition otherPlayerTurn;
 
     public Dealer() {
-        displayArea = new JTextArea();
-        add(displayArea, BorderLayout.CENTER);
+        super("Dealer (Network Server)");
+
+        runGame = Executors.newFixedThreadPool(2);
+        gameLock = new ReentrantLock();
+        otherPlayerConnected = gameLock.newCondition();
+        otherPlayerTurn = gameLock.newCondition();
+
+        // TODO: Logic to instantiate shuffled deck
+
+        players = new Player[2];
+        currentPlayer = P_ONE;
+
+        try {
+            server = new ServerSocket(23516, 2); // Setup socket
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
+            System.exit(1);
+        }
+
+        outputArea = new JTextArea();
+        add(outputArea, BorderLayout.CENTER);
+        outputArea.setText("Server awaiting connections...\n");
         setSize(400, 400);
         setVisible(true);
 
+    }
+
+    public void execute() {
+        try {
+            players[0] = new Player(server.accept(), );
+        }
     }
 
     public void runServer() {
@@ -97,17 +134,42 @@ public class Dealer extends JFrame {
         }
     }
 
-    private void print(String message){
-        displayArea.append("\n"+message);
+    private void print(String message) {
+        outputArea.append("\n" + message);
     }
 
-    private class Player {
-        private int playerNum;
-        private Card[] currentCards;
+    private class Player implements Runnable {
+        private Socket connection;
+        private Scanner input;
+        private Formatter output;
+        private String playerName;
+        // TODO: Do I need this? Feel like Locks and all that should take care of it
+        private boolean suspended = true; // whether thread is suspended
 
-        public Player(int num, Card initDeal[]) {
-            int playerNum = num;
-            currentCards = initDeal;
+        // TODO: Make this the hand object. replace all other card objects with hand
+        private Card[] heldCards;
+
+        // TODO: Change this initial Deal to an initial Hand
+        public Player(Socket socket, String name, Card initDeal[]) {
+            playerName = name;
+            heldCards = initDeal;
+            connection = socket;
+
+            try {
+                input = new Scanner(connection.getInputStream());
+                output = new Formatter(connection.getOutputStream());
+            } catch (IOException ioException) {
+                ioException.printStackTrace();
+                System.exit(1);
+            }
+        }
+
+        // TODO:
+        // public void opponentHandupdate(Hand newHand)
+
+        public void run() {
+            displayMessage(String.format("%s connected", playerName));
+            // TODO NEXT: side by side tic tac and fill in dealer
         }
 
         public boolean isOver() {
@@ -116,15 +178,12 @@ public class Dealer extends JFrame {
         }
     }
 
-    private void firstDeal() {
-
+    private void displayMessage(final String message) {
+        SwingUtilities.invokeLater(
+                () -> {
+                    outputArea.append(message+"\n"); // add message
+                }
+        );
     }
 
-    private void deal(Player player) {
-
-    }
-
-    private void hit(Player player) {
-
-    }
 }
